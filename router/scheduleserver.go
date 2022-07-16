c0b2a3a795d33c68850c2dd6f5280fa927996433
LMF709268224 <709268224@qq.com>
2022-07-16 19:27:46 +0800

    .

--- router/scheduleserver.go
+++ router/scheduleserver.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"titan-ultra-network/log"
-	"titan-ultra-network/service/schedule"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/sirupsen/logrus"
@@ -15,8 +14,6 @@ func StartScheduleServer(port string) {
 	router := httprouter.New()
 	// 测试
 	router.GET("/test", test)
-
-	router.GET("/ws/:deviceid", schedule.AcceptWebsocket)
 
 	logrus.Fatal(http.ListenAndServe(port, router))
 
