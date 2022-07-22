2f3dbd1cad0879700b3eb30f94eedcac13b9b6dd
LMF709268224 <709268224@qq.com>
2022-07-22 19:12:44 +0800

    注释

--- node/fsutil/filesize_unix.go
+++ node/fsutil/filesize_unix.go
@@ -3,6 +3,7 @@ package fsutil
 import (
 	"os"
 	"path/filepath"
+	"syscall"
 
 	"golang.org/x/xerrors"
 )
@@ -19,17 +20,16 @@ func FileSize(path string) (SizeInfo, error) {
 		if err != nil {
 			return err
 		}
-		// TODO open
-		// if !info.IsDir() {
-		// 	stat, ok := info.Sys().(*syscall.Stat_t)
-		// 	if !ok {
-		// 		return xerrors.New("FileInfo.Sys of wrong type")
-		// 	}
+		if !info.IsDir() {
+			stat, ok := info.Sys().(*syscall.Stat_t)
+			if !ok {
+				return xerrors.New("FileInfo.Sys of wrong type")
+			}
 
-		// 	// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
-		// 	//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
-		// 	size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
-		// }
+			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
+			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
+			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
+		}
 		return err
 	})
 	if err != nil {
