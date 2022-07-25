e64cf5cabd8014fcf563430c0ff58d6d5fc8be3b
riguang <zscboy@gmail.com>
2022-07-25 16:38:48 +0800

    file store and rocksdb

--- go.mod
+++ go.mod
@@ -67,6 +67,7 @@ require (
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/prometheus/statsd_exporter v0.21.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
---
+++ stores/filesystem.go
@@ -0,0 +1,28 @@
+package stores
+
+import (
+	"os"
+	"path/filepath"
+)
+
+type fileStore struct {
+	Path string
+}
+
+func (fs fileStore) Type() string {
+	return "FileStore"
+}
+
+func (fs fileStore) Put(data []byte, cid string) error {
+	return os.WriteFile(fs.Path, data, 0644)
+}
+
+func (fs fileStore) Get(cid string) ([]byte, error) {
+	filePath := filepath.Join(fs.Path, cid)
+	return os.ReadFile(filePath)
+}
+
+func (fs fileStore) Delete(cid string) error {
+	filePath := filepath.Join(fs.Path, cid)
+	return os.Remove(filePath)
+}
