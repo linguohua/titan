0df3b957aa100ce8ffe70474b42d39272a1dd95d
LMF709268224 <709268224@qq.com>
2022-07-26 16:23:33 +0800

    data

--- api/api_scheduler.go
+++ api/api_scheduler.go
@@ -5,5 +5,9 @@ import "context"
 type Scheduler interface {
 	Common
 
-	EdgeNodeConnect(context.Context, string) error
+	EdgeNodeConnect(context.Context, string) error //perm:read
+
+	CacheData(context.Context, []string, []string) error //perm:read
+
+	LoadData(context.Context, string, string) ([]byte, error) //perm:read
 }
