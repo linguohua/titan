98cb2555a827988283ee4de40c90c94fbc8ffdd6
riguang <zscboy@gmail.com>
2022-07-27 16:41:48 +0800

    linux build

--- cli/candidate.go
+++ cli/candidate.go
@@ -2,7 +2,6 @@ package cli
 
 import (
 	"fmt"
-	"syscall"
 
 	"github.com/urfave/cli/v2"
 )
@@ -34,11 +33,6 @@ var VerfyDataCmd = &cli.Command{
 		// TODO: print more useful things
 
 		cid, err := api.VerifyData(ctx, fid, "http://localhost:1234/rpc/v0")
-		if err == syscall.ERROR_FILE_NOT_FOUND {
-			fmt.Println("file not found")
-			return err
-		}
-
 		if err != nil {
 			fmt.Println("err", err)
 			return err
