ac6c0d2ad3f4934b4d3b5dada102da7cc034885f
LMF709268224 <709268224@qq.com>
2022-10-20 10:09:41 +0800

    Permission
    (cherry picked from commit 40a2443f17888f53995674cefec2e5d95e7fd27b)

--- api/api_scheduler.go
+++ api/api_scheduler.go
@@ -21,11 +21,12 @@ type Scheduler interface {
 	ShowDataInfos(ctx context.Context, cid string) (string, error)                               //perm:admin
 
 	// call by node
-	EdgeNodeConnect(ctx context.Context, deviceID string) error                                        //perm:read
 	DeleteBlockRecords(ctx context.Context, deviceID string, cids []string) (map[string]string, error) //perm:read
-	ValidateBlockResult(ctx context.Context, validateResults ValidateResults) error                    //perm:read
-	CandidateNodeConnect(ctx context.Context, deviceID string) error                                   //perm:read
-	CacheResult(ctx context.Context, deviceID string, resultInfo CacheResultInfo) (string, error)      //perm:read
+	EdgeNodeConnect(ctx context.Context, deviceID string) error                                        //perm:write
+	ValidateBlockResult(ctx context.Context, validateResults ValidateResults) error                    //perm:write
+	CandidateNodeConnect(ctx context.Context, deviceID string) error                                   //perm:write
+	CacheResult(ctx context.Context, deviceID string, resultInfo CacheResultInfo) (string, error)      //perm:write
+
 	// call by user
 	FindNodeWithBlock(ctx context.Context, cid string, ip string) (string, error)                             //perm:read
 	GetDownloadInfoWithBlocks(ctx context.Context, cids []string, ip string) (map[string]DownloadInfo, error) //perm:read
