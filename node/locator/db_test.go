b5a6ee9a2a1a1007f9dea00c1718f8d838eb3b20
riguang <zscboy@gmail.com>
2022-11-04 16:23:16 +0800

    fix get device info

--- node/locator/db_test.go
+++ node/locator/db_test.go
@@ -1,20 +1,23 @@
 package locator
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestDB(t *testing.T) {
-	url := "user01:sql001@tcp(127.0.0.1:3306)/location"
+	url := "user01:sql001@tcp(127.0.0.1:3306)/locator"
 	db := newDB(url)
 	defer db.close()
 
-	err := db.addAccessPoints("sz", "http://192.168.0.1:1080/rpc/v0", 100, "111111111111111")
-	if err != nil {
-		fmt.Printf("err:%s", err.Error())
-		return
-	}
+	// info, err := db.getDeviceInfo("525e7729506711ed8c2c902e1671f843")
+	// if err != nil {
+	// 	fmt.Printf("err:%s", err.Error())
+	// 	return
+	// }
+	// 525e7729506711ed8c2c902e1671f843	http://192.168.0.26:3456/rpc/v0	CN-GD-Shenzhen	1
+	db.db.setDeviceInfo("525e7729506711ed8c2c902e1671f843", "http://192.168.0.29:3456/rpc/v0", "CN-GD-Shenzhenabc", false)
+	// l := &Locator{db: db}
+	// l.DeviceOffline(nil, info.DeviceID)
 	// db.addAccessPoints("gz", "http://localhost:1080/rpc/v0", 100)
 	// db.removeAccessPoints("sz")
 	// areaIDs, err := db.listAccessPoints()
@@ -22,6 +25,6 @@ func TestDB(t *testing.T) {
 	// 	fmt.Printf("err:%s", err.Error())
 	// 	return
 	// }
-	// fmt.Printf("areaID %v", areaIDs)
+	// fmt.Printf("info %v", *info)
 
 }
