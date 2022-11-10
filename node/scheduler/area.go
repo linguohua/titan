6267770e86e1cc7cbf14da67bb950b32a3f1ca02
LMF709268224 <709268224@qq.com>
2022-11-10 14:57:25 +0800

    local ip address

--- node/scheduler/area.go
+++ node/scheduler/area.go
@@ -1,12 +1,14 @@
 package scheduler
 
 import (
+	"regexp"
+
 	"github.com/linguohua/titan/region"
 )
 
 var (
 	serverArea = "CN-GD-Shenzhen"
-	whitelist  = []string{"192.168.0.26"}
+	whitelist  = []string{}
 )
 
 // InitServerArea set area
@@ -28,6 +30,12 @@ func areaExist(area string) bool {
 
 func ipLegality(ip string) (bool, *region.GeoInfo) {
 	geoInfo, _ := region.GetRegion().GetGeoInfo(ip)
+
+	if ipAddrACL(ip) {
+		geoInfo.Geo = serverArea
+		return true, geoInfo
+	}
+
 	if areaExist(geoInfo.Geo) {
 		return true, geoInfo
 	}
@@ -40,4 +48,28 @@ func ipLegality(ip string) (bool, *region.GeoInfo) {
 	}
 
 	return false, geoInfo
+}
+
+func ipAddrACL(ip string) bool {
+	if match0, _ := regexp.MatchString(`127\.0\.0\.1`, ip); match0 {
+		return true
+	}
+
+	if match2, _ := regexp.MatchString(`10\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])`, ip); match2 {
+		if match3, _ := regexp.MatchString(`10\.10\.30\.1`, ip); match3 {
+			return false
+		}
+		return true
+	}
+
+	if match4, _ := regexp.MatchString(`172\.((1[6-9])|(2[0-9])|(3[0-1]))\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])`, ip); match4 {
+		return true
+	}
+
+	if match5, _ := regexp.MatchString(
+		`192\.168\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])`, ip); match5 {
+		return true
+	}
+
+	return false
 }
