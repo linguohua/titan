38d152e143aed50d90b60247aee075a18c2a2a54
limengjun <18825899896@163.com>
2022-12-09 15:51:16 +0800

    adjust database structure

--- node/scheduler/db/persistent/sql_test.go
+++ node/scheduler/db/persistent/sql_test.go
@@ -14,8 +14,32 @@ func TestSqlDB_InsertValidateResultInfo(t *testing.T) {
 		return
 	}
 	info := new(ValidateResult)
+	info.DeviceID = "001"
+	info.ValidatorID = "1"
+	info.RoundID = 100
+	info.ServerName = "test"
+	info.Status = ValidateStatusCreate.Int()
 	info.StartTime = time.Now()
 	err = db.InsertValidateResultInfo(info)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+}
+
+func TestSqlDB_UpdateValidateResultInfo(t *testing.T) {
+	db, err := InitSQL(url)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	info := new(ValidateResult)
+	info.DeviceID = "001"
+	info.RoundID = 100
+	info.Msg = "ok"
+	info.EndTime = time.Now()
+	info.Status = ValidateStatusSuccess.Int()
+	err = db.UpdateValidateResultInfo(info)
 	if err != nil {
 		t.Error(err.Error())
 		return
