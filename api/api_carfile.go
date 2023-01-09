e5a4fb04f87d891f75de0789261cbbd053fe8fc4
riguang <zscboy@gmail.com>
2023-01-09 19:52:19 +0800

    api for cache carfile

---
+++ api/api_carfile.go
@@ -0,0 +1,26 @@
+package api
+
+import "context"
+
+type CarfileOperation interface {
+	// cache carfile
+	CacheCarfile(ctx context.Context, carfileCID string, sources []*DowloadSource) (CacheCarfileResult, error) //perm:write
+	// delete carfile
+	DeleteCarfile(ctx context.Context, carfileCID string) error //perm:write
+	// delete all carfiles
+	DeleteAllCarfiles(ctx context.Context) error //perm:admin
+	// delete carfile with wait cache
+	DeleteWaitCacheCarfile(ctx context.Context, carfileCID string) error //perm:admin
+}
+
+type CacheCarfileResult struct {
+	CacheCarfileCount    int
+	WaitCacheCarfileNum  int
+	DoingCacheCarfileNum int
+	DiskUsage            float64
+}
+
+type DowloadSource struct {
+	CandidateURL   string
+	CandidateToken string
+}
