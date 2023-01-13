ee44b80cca4aea4ff023a97326629141bf164560
riguang <zscboy@gmail.com>
2023-01-13 18:34:32 +0800

    download carfile

--- node/carfile/carfile_encode.go
+++ node/carfile/carfile_encode.go
@@ -70,6 +70,7 @@ func ecodeWaitList(carfiles []*carfile) ([]byte, error) {
 			CarfileSize:               cf.carfileSize,
 			DownloadSize:              cf.downloadSize}
 		waitList = append(waitList, encodeCarfile)
+		log.Infof("encodeCarfile %v", encodeCarfile)
 	}
 
 	var buffer bytes.Buffer
@@ -104,7 +105,7 @@ func decodeWaitListFromData(carfileData []byte) ([]*carfile, error) {
 		cf.downloadSize = encodeCarfile.DownloadSize
 
 		cfs = append(cfs, cf)
-		log.Infof("encodeCarfile %v", encodeCarfile)
+		log.Infof("decodeWaitListFromData %v", encodeCarfile)
 	}
 
 	return cfs, nil
