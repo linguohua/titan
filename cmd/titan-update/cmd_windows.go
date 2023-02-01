39374f3f9ca4227021a861208f7bf26376db5369
riguang <zscboy@gmail.com>
2023-02-01 17:35:45 +0800

    update app

---
+++ cmd/titan-update/cmd_windows.go
@@ -0,0 +1,11 @@
+package main
+
+func killApp(appName string) error {
+	// cmd := exec.Command("pkill", appName)
+	// _, err := cmd.Output()
+	// if err != nil {
+	// 	return err
+	// }
+
+	return nil
+}
