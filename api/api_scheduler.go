a60162a7a2bfdb8918464597e43d0cecdcaadebc
riguang <zscboy@gmail.com>
2023-02-23 15:31:42 +0800

    api perm

--- api/api_scheduler.go
+++ api/api_scheduler.go
@@ -60,7 +60,7 @@ type Scheduler interface {
 	GetEdgeExternalAddr(ctx context.Context, deviceID, schedulerURL string) (string, error) //perm:write
 	// nat travel
 	CheckEdgeIfBehindFullConeNAT(ctx context.Context, edgeURL string) (bool, error) //perm:read
-	GetNatType(ctx context.Context, deviceID string) (string, error)
+	GetNatType(ctx context.Context, deviceID string) (string, error)                //perm:write
 
 	// call by user
 	GetDownloadInfosWithCarfile(ctx context.Context, cid, publicKey string) ([]*DownloadInfoResult, error) //perm:read
