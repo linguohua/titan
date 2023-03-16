4898cf0326e19dc0a3d1bd9a0d2eafaca70a29b3
riguang <zscboy@gmail.com>
2023-03-16 17:50:03 +0800

    change carfile hash to cid

--- node/gateway/handler_car.go
+++ node/gateway/handler_car.go
@@ -1,11 +1,22 @@
 package gateway
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-libipfs/blocks"
 	"github.com/linguohua/titan/api/types"
 )
 
 func (gw *Gateway) serveCar(w http.ResponseWriter, r *http.Request, ticket *types.AccessTicket, carVersion string) {
 
+}
+
+type dagStore struct {
+	gw *Gateway
+}
+
+func (ds dagStore) Get(ctx context.Context, c cid.Cid) (blocks.Block, error) {
+	return ds.gw.getBlock(ctx, c)
 }
