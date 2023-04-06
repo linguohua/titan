c0b605aaef321857705be4fc13ec57f38dee3070
LMF709268224 <709268224@qq.com>
2023-04-06 18:35:54 +0800

    note node

--- node/scheduler/node/impl.go
+++ node/scheduler/node/impl.go
@@ -4,11 +4,11 @@ import (
 	"github.com/linguohua/titan/api/types"
 )
 
-// NodesQuit Nodes quit
+// NodesQuit nodes quit and removes their replicas
 func (m *Manager) NodesQuit(nodeIDs []string) {
 	err := m.SetNodesQuitted(nodeIDs)
 	if err != nil {
-		log.Errorf("NodeExited SetNodesQuit err:%s", err.Error())
+		log.Errorf("QuitNodes: SetNodesQuitted err:%s", err.Error())
 		return
 	}
 
@@ -16,23 +16,23 @@ func (m *Manager) NodesQuit(nodeIDs []string) {
 
 	hashes, err := m.FetchAssetHashesOfNodes(nodeIDs)
 	if err != nil {
-		log.Errorf("LoadAssetHashesOfNodes err:%s", err.Error())
+		log.Errorf("QuitNodes: FetchAssetHashesOfNodes err:%s", err.Error())
 		return
 	}
 
 	err = m.DeleteReplicasForNodes(nodeIDs)
 	if err != nil {
-		log.Errorf("RemoveReplicaInfoWithNodes err:%s", err.Error())
+		log.Errorf("QuitNodes: DeleteReplicasForNodes err:%s", err.Error())
 		return
 	}
 
 	for _, hash := range hashes {
-		log.Infof("need to add replica :%s", hash)
+		log.Infof("QuitNodes: Need to add replica for asset:%s", hash)
 	}
 }
 
-// GetAllCandidates get all candidates
-func (m *Manager) GetAllCandidates() []string {
+// GetAllCandidateNodes  returns a list of all candidate nodes
+func (m *Manager) GetAllCandidateNodes() []string {
 	var out []string
 	m.candidateNodes.Range(func(key, value interface{}) bool {
 		nodeID := key.(string)
@@ -43,7 +43,7 @@ func (m *Manager) GetAllCandidates() []string {
 	return out
 }
 
-// GetNode get node
+// GetNode retrieves a node with the given node ID
 func (m *Manager) GetNode(nodeID string) *Node {
 	edge := m.GetEdgeNode(nodeID)
 	if edge != nil {
@@ -58,7 +58,7 @@ func (m *Manager) GetNode(nodeID string) *Node {
 	return nil
 }
 
-// GetEdgeNode Get EdgeNode
+// GetEdgeNode retrieves an edge node with the given node ID
 func (m *Manager) GetEdgeNode(nodeID string) *Node {
 	nodeI, exist := m.edgeNodes.Load(nodeID)
 	if exist && nodeI != nil {
@@ -70,7 +70,7 @@ func (m *Manager) GetEdgeNode(nodeID string) *Node {
 	return nil
 }
 
-// GetCandidateNode Get Candidate Node
+// GetCandidateNode retrieves a candidate node with the given node ID
 func (m *Manager) GetCandidateNode(nodeID string) *Node {
 	nodeI, exist := m.candidateNodes.Load(nodeID)
 	if exist && nodeI != nil {
@@ -82,7 +82,7 @@ func (m *Manager) GetCandidateNode(nodeID string) *Node {
 	return nil
 }
 
-// GetOnlineNodeList get nodes with type
+// GetOnlineNodeList returns a list of online nodes of the given type
 func (m *Manager) GetOnlineNodeList(nodeType types.NodeType) ([]string, error) {
 	list := make([]string, 0)
 	limit := 100
@@ -119,7 +119,7 @@ func (m *Manager) GetOnlineNodeList(nodeType types.NodeType) ([]string, error) {
 	return list, nil
 }
 
-// NodeOnline node online
+// NodeOnline registers a node as online
 func (m *Manager) NodeOnline(node *Node) error {
 	nodeID := node.NodeInfo.NodeID
 
@@ -129,12 +129,12 @@ func (m *Manager) NodeOnline(node *Node) error {
 	}
 
 	if node.NodeType == types.NodeEdge {
-		m.storeEdge(node)
+		m.storeEdgeNode(node)
 		return nil
 	}
 
 	if node.NodeType == types.NodeCandidate {
-		m.storeCandidate(node)
+		m.storeCandidateNode(node)
 		// update validator owner
 		return m.UpdateValidatorInfo(m.ServerID, nodeID)
 	}
